3b8e5c166f9576516899fa41c4ba214d6bb50cd0
Sebastian Deppisch <s.deppisch@swannekke.de>
2017-07-07 01:26:35 +0200

    first commit

---
+++ youtubeSubscriberDisplay.ino
@@ -0,0 +1,75 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>   
+#include <WiFiManager.h> 
+#include <ESP8266HTTPClient.h>
+
+#include "LedControl.h"
+#include "ArduinoJson.h"
+
+
+//put the name of the channel and your google-api key here
+const String CHANNEL = "";
+const String APIKEY = "";
+//---- 
+
+
+const String API="https://www.googleapis.com/youtube/v3/channels?part=statistics";
+const char* FINGERPRINT = "D1 9C 65 C3 64 87 7E 97 93 9E A0 83 0E C3 8D 7C 98 72 DC 48"; //SHA1 fingerprint of www.googleapis.com
+LedControl lc=LedControl(13,14,16,1);
+void setup() {
+  Serial.begin(9600);
+  
+  lc.shutdown(0,false); //wakeup
+  lc.setIntensity(0,8); //brightness
+  lc.clearDisplay(0); //clear
+
+  WiFiManager wifiManager; //use wifiManager to configure wifi ssid and password
+  wifiManager.autoConnect();
+}
+
+uint32_t tenPow(uint8_t exponent){
+    uint32_t result = 1;
+    while (exponent--){
+      result *= 10;
+    }
+    return result;
+}
+
+void setNumber(uint32_t number){
+  for(uint8_t i=0;i<8;i++){
+    uint32_t n=number;
+    uint32_t exponent = tenPow(i);
+    n = n / exponent;
+    n = n % 10;
+    if(i > 0 && n==0){ //dont display leading zeros
+      n = 16; // 16 e.g. is all off
+    }
+    lc.setDigit(0, i, n, false);
+  }
+}
+
+bool firstTry = true;
+void loop() { 
+  if((WiFi.status() == WL_CONNECTED)) {
+      HTTPClient http;
+      http.begin(API+"&forUsername="+CHANNEL+"&key="+APIKEY, FINGERPRINT);
+      int httpCode = http.GET();
+      if(httpCode > 0) {
+          if(httpCode == HTTP_CODE_OK) {
+              String payload = http.getString();
+              Serial.println(payload);
+              StaticJsonBuffer<1000> jsonBuffer;
+              JsonObject& root = jsonBuffer.parseObject(payload);
+              setNumber(root["items"][0]["statistics"]["subscriberCount"]);
+              firstTry = false;
+          }
+      }else{
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+  }
+  if(!firstTry){ //there seems - sometimes- to a be problem on checking the fingerprint. So, just for the first try, dont wait between (failed) polls
+    delay(60000);
+  }
+  
+}
